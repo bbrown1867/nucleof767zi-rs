64b72b68d371d7e23310d33547a975892a6f1bdd
Ben Brown <5721984+bbrown1867@users.noreply.github.com>
2020-07-22 15:45:03 -0400

    Use semihosting for print

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # nucleo144-rs
 
-Basic Rust board support package for the [Nucleo-144 development board](https://www.st.com/en/evaluation-tools/nucleo-f767zi.html) targeting the STM32F767ZI microcontroller. The current goal is to get familiar with the embedded Rust ecosystem, rather than provide any real package.
+Basic Rust board support package for the [Nucleo-144 development board](https://www.st.com/en/evaluation-tools/nucleo-f767zi.html) targeting the STM32F767ZI microcontroller. The current goal is to get familiar with the embedded Rust ecosystem, rather than provide any real package. Uses [stm32f7xx_hal](https://github.com/stm32-rs/stm32f7xx-hal/).
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
+extern crate panic_semihosting;
 
 use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
 use stm32f7xx_hal::{
     device,
     delay::Delay,
@@ -25,14 +26,16 @@ fn main() -> ! {
     let mut led3 = gpiob.pb14.into_push_pull_output();
 
     loop {
-        led1.set_high().expect("GPIO can never fail");
-        led2.set_high().expect("GPIO can never fail");
-        led3.set_high().expect("GPIO can never fail");
+        led1.set_high().unwrap();
+        led2.set_high().unwrap();
+        led3.set_high().unwrap();
         delay.delay_ms(1000_u16);
 
-        led1.set_low().expect("GPIO can never fail");
-        led2.set_low().expect("GPIO can never fail");
-        led3.set_low().expect("GPIO can never fail");
+        led1.set_low().unwrap();
+        led2.set_low().unwrap();
+        led3.set_low().unwrap();
         delay.delay_ms(1000_u16);
+
+        hprintln!("Hello World!\r\n").unwrap();
     }
 }
